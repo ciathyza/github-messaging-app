9cb900c9803e539de2300b5a49b98bcd523c3de7
ciathyza <ciathyza@ciathyza.com>
2018-10-08 00:38:19 +0900

    Rounded images in TableView :)

--- Github Messaging App/Github Messaging App/Source/Extensions/UIImageView.swift
+++ Github Messaging App/Github Messaging App/Source/Extensions/UIImageView.swift
@@ -12,19 +12,28 @@ import UIKit
 let imageCache = NSCache<NSString, UIImage>()
 
 
+///
+/// Extensions for UIImageView.
+///
 extension UIImageView
 {
+	///
+	/// Loads an image from a URL into the image view. If the image is already cached
+	/// it will be pulled from the cache.
+	///
 	func loadImageUsingCache(withUrl urlString:String)
 	{
 		let url = URL(string: urlString)
 		if url == nil { return }
-		self.image = nil
+		//self.image = nil
+		setRoundedImage(nil)
 		
 		// check cached image
 		if let cachedImage = imageCache.object(forKey: urlString as NSString)
 		{
 			Log.debug("APP", "Reusing cached image for \(urlString).")
-			self.image = cachedImage
+			//self.image = cachedImage
+			setRoundedImage(cachedImage)
 			return
 		}
 		
@@ -46,11 +55,25 @@ extension UIImageView
 				if let image = UIImage(data: data!)
 				{
 					imageCache.setObject(image, forKey: urlString as NSString)
-					self.image = image
+					//self.image = image
+					self.setRoundedImage(image)
 					activityIndicator.removeFromSuperview()
 				}
 			}
 			
 		}).resume()
+	}
+	
+	
+	func setRoundedImage(_ image:UIImage?)
+	{
+		self.image = image
+		if self.image != nil
+		{
+			/* Frame width and height needs to be the same or the the rounded image
+			   will look strange. */
+			self.layer.masksToBounds = true
+			self.layer.cornerRadius = self.frame.height / 2.0
+		}
 	}
 }
